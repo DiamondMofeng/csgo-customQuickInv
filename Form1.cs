0f359eccee3a0cbed655bcb68d4981b214e21004
Mofeng <363034937@qq.com>
2021-07-13 02:54:34 +0800

    v0.8.0

--- Form1.cs
+++ Form1.cs
@@ -2,19 +2,6 @@
 using System.IO;
 using System.Drawing;
 using System.Text.RegularExpressions;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing.Imaging;
-using System.Drawing.Drawing2D;
-using System.Drawing.Text;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Globalization;
-using System.Web;
-using System.Resources;
-using winformLearn.Properties;
 using System.Windows.Forms;
 
 
---
+++ README.md
@@ -0,0 +1,19 @@
+# csgo-customQuickInv
+
+#csgo武器轮盘可视化编辑器
+
+csgo-QuickInventoryVisuableEditor
+
+
+
+
+work in progress
+
+接近完成了
+
+- [ x ] 基本界面设计
+- [ x ] 基本功能实现
+- [ ] 导入、存档功能
+- [ ] 其他小功能
+
+
